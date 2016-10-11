1e244843497648ae9e969ec7ec3dfd63b96edf9a
EDA <eda@mac-mini-6.local>
2016-10-11 17:16:36 +1300

    working with d3

--- client/components/ChampListV2.jsx
+++ client/components/ChampListV2.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router'
 export default React.createClass({
   render () {
     return (
-      <div className="champContainer2"
+      <div className="champContainer2">
         <img src={champ.image}/>
         <Link className="link" to={`/champion/${champ.id}`}> {champ.name} </Link>
       </div>
